b45d905ae6d6a1a64496b001b506a07f742ec268
janus_wel <janus.wel.3@gmail.com>
2016-05-27 22:08:58 +0900

    add guard clause

--- src/model.js
+++ src/model.js
@@ -2,6 +2,8 @@
 
 export default class Model {
     constructor () {
-        throw TypeError('do not create an instance of Model.');
+        if (this.constructor.name === 'Model') {
+            throw TypeError('do not create an instance of Model.');
+        }
     }
 }
